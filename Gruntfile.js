2138039e77bafa0b05c3609681dd62ce51d1ef6c
Joseph Weidinger <jsphweid@gmail.com>
2016-11-21 18:12:06 -0600

    redundant uglify in file

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,6 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // uglify
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
 
         // copy
         copy: {
